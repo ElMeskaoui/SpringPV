32a864d313c1ca9cc668a16b3a1c7e8c19555fce
Ikram Damri <damri.ikram@gmail.com>
2022-06-04 18:17:38 +0100

    set TypePlaints Controller

--- src/main/java/com/example/demo/controller/TypePlaintsController.java
+++ src/main/java/com/example/demo/controller/TypePlaintsController.java
@@ -1,4 +1,52 @@
 package com.example.demo.controller;
 
+import com.example.demo.entities.TypePlaints;
+import com.example.demo.service.TypePlaintsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class TypePlaintsController {
+
+    @Autowired
+    private TypePlaintsService typePlaintsService;
+
+    //Get all types of plaints
+    @GetMapping("/typeplaints")
+    public List<TypePlaints> findAllTypePlaints() {
+        return typePlaintsService.getTypePlaints();
+    }
+
+    @GetMapping("/typeplaintsById/{id}")
+    public TypePlaints findTypePlaintsById(@PathVariable int id) {
+        return typePlaintsService.getTypePlaintsById(id);
+    }
+
+    @GetMapping("/typeplaints/{name}")
+    public TypePlaints findTypePlaintsByName(@PathVariable String name) {
+        return typePlaintsService.getTypePlaintsByName(name);
+    }
+
+    // POST
+    @PostMapping("/addTypePlaints")
+    public TypePlaints addTypePlaints(@RequestBody TypePlaints typePlaints) {
+        return typePlaintsService.saveTypePlaints(typePlaints);
+    }
+
+
+    // Update
+    @PutMapping("/update")
+    public TypePlaints updateTypePlaints(@RequestBody TypePlaints typePlaints) {
+        return typePlaintsService.updateTypePlaints(typePlaints);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteTypePlaints(@PathVariable int id) {
+        return typePlaintsService.deleteTypePlaints(id);
+    }
+
+
+
 }
